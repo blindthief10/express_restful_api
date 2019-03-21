7f4c9ba807adf8c5f47f507e420786b0754eae35
blindthief10 <kostasdiakogiannis23@gmail.com>
2019-03-21 10:53:31 +0100

    getting by company added

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/orders', (req, res, next) => {
-  next();
-})
-
 app.post('/orders', (req, res, next) => {
   orders.push(req.body);
   next();
@@ -18,6 +14,17 @@ app.post('/orders', (req, res, next) => {
 app.use('/orders', (req, res, next) => {
   res.status(200).send(orders);
 })
+
+app.get('/orders/:firma', (req, res, next) => {
+  const firma = req.params.firma; // apple, htc,
+  const filteredByFirma = orders.filter(product => product.productsFirma.toLowerCase() === firma.toLowerCase());
+  if (!filteredByFirma.length) {
+    res.status(404).send('The company you set doesnt exist!')
+  } else {
+    res.status(200).send(filteredByFirma);
+  }
+})
+
 
 app.listen(4000);
 console.log('Server is listening to port 4000');
