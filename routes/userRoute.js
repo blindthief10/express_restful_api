ec685fd3084aa1297ae2c8d62e00dcf10520fb1b
blindthief10 <kostasdiakogiannis23@gmail.com>
2019-03-27 10:59:39 +0100

    adding validation functions trimming etc

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -20,9 +20,9 @@ usersRoute.get('/:username', (req, res, next) => {
 })
 
 usersRoute.post('/', [
-  check(['firstName', 'lastName', 'password', 'email', 'userName']).exists().withMessage('All fields are required!'),
-  check('email').isEmail().withMessage('The email you have passed is invalid'),
-  check('password').isLength({min: 4, max: 20}).withMessage('The password field must be between 4 and 20 characters')
+  check(['firstName', 'lastName', 'password', 'email', 'userName']).exists().not().isEmpty().withMessage('All fields are required!'),
+  check('email').trim().isEmail().normalizeEmail().withMessage('The email you have passed is invalid'),
+  check('password').trim().isLength({min: 4, max: 20}).escape().withMessage('The password field must be between 4 and 20 characters')
 ], (req, res, next) => {
   const errors = validationResult(req);
 
