1335ae0ff6144a113132051f3f7b2c11e46612bb
blindthief10 <kostasdiakogiannis23@gmail.com>
2019-04-24 12:08:52 +0200

    add a user, post request implemented, version key excluded from schema

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,7 +6,7 @@ const userModel = require('../models/userModel');
 usersRoute.get('/', async (req, res, next) => {
 
   try {
-    const allUsers = await userModel.find();
+    const allUsers = await userModel.find({}, {firstName: 1, _id: 0});
     res.status(200).send(allUsers);
   } catch (error) {
     next(error);
@@ -24,37 +24,30 @@ usersRoute.get('/:username', async (req, res, next) => {
   }
 
 })
-//
-// usersRoute.post('/', [
-//   check(['firstName', 'lastName', 'password', 'email', 'userName']).exists().not().isEmpty().withMessage('All fields are required!'),
-//   check('email').trim().isEmail().normalizeEmail().withMessage('The email you have passed is invalid'),
-//   check('password').trim().isLength({min: 4, max: 20}).escape().withMessage('The password field must be between 4 and 20 characters'),
-//   check('email').custom(emailValue => {
-//
-//     const foundUserByThisEmail = users.find(user => user.email === emailValue);
-//
-//     if (foundUserByThisEmail) {
-//       throw new Error(`A user with email address ${emailValue} already exists!`);
-//     }else {
-//       return true;
-//     }
-//
-//   })
-// ], (req, res, next) => {
-//   const errors = validationResult(req);
-//
-//   if (errors.isEmpty()) {
-//     users.push(req.body);
-//     res.status(201).json({
-//       message: 'The user was created succesfully!'
-//     });
-//   }else {
-//     const errorCase = new Error(errors.array()[0].msg);
-//     errorCase.status = 403;
-//     next(errorCase);
-//   }
-//
-// })
+
+usersRoute.post('/', [
+  check(['firstName', 'lastName', 'password', 'email', 'userName']).exists().not().isEmpty().withMessage('All fields are required!'),
+  check('email').trim().isEmail().normalizeEmail().withMessage('The email you have passed is invalid'),
+  check('password').trim().isLength({min: 4, max: 20}).escape().withMessage('The password field must be between 4 and 20 characters')
+], async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (errors.isEmpty()) {
+
+    try {
+      await userModel.create(req.body);
+      res.status(201).json({message: 'The user was created succesfully!'});
+    } catch (error) {
+      next(error);
+    }
+
+  }else {
+    const errorCase = new Error(errors.array()[0].msg);
+    errorCase.status = 403;
+    next(errorCase);
+  }
+
+})
 
 usersRoute.put('/:username', async (req, res, next) => {
     // Update a document by username
--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -6,6 +6,6 @@ const userSchema = new mongoose.Schema({
   userName: {type: String, required: true},
   email: {type: String, required: true},
   password: {type: String, required: true}
-})
+}, {versionKey: false})
 
 module.exports = userSchema;
